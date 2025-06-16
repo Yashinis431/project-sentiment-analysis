da95e02a9f96067db993cd0584e3049102f0e7aa
Yashini S <yashinis431@gmail.com>
2025-06-16 17:10:14 +0530

    Delete model.py

--- model.py
+++
@@ -1,9 +0,0 @@
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
-
-def train_and_evaluate(X_train, X_test, y_train, y_test):
-    model = MultinomialNB()
-    model.fit(X_train, y_train)
-    predictions = model.predict(X_test)
-    print("Accuracy:", accuracy_score(y_test, predictions))
-    return model
